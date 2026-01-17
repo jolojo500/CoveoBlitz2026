7a6bbbe3756e6a5939697e803d49588ad6fa15c5
yanis <chabaneyanis52@gmail.com>
2026-01-17 10:41:51 -0500

    New map system

--- java/src/main/java/codes/blitz/game/bot/Bot.java
+++ java/src/main/java/codes/blitz/game/bot/Bot.java
@@ -16,6 +16,14 @@ public class Bot {
    * Here is where the magic happens, for now the moves are not very good. I bet you can do better ;)
    */
   public List<Action> getActions(TeamGameState gameMessage) {
+    int[][] nutri = gameMessage.world().map().nutrientGrid();
+    for (int i = 0; i < nutri[0].length; i++) {
+      for (int j = 0; j < nutri[1].length; j++) {
+        System.out.print(nutri[i][j]);
+      }
+      System.out.println();
+    }
+
     List<Action> actions = new ArrayList<>();
 
     TeamInfo myTeam = gameMessage.world().teamInfos().get(gameMessage.yourTeamId());
